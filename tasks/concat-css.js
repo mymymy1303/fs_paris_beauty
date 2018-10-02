3ac5562045dee6931258f65846c1b9be7a1a7a3b
mymymy1303 <thehuy160897@gmail.com>
2018-10-03 01:38:47 +0800

    Add Gallery-service component

--- tasks/concat-css.js
+++ tasks/concat-css.js
@@ -4,6 +4,8 @@ module.exports = function (gulp, $, browserSync) {
 				'bower_components/font-awesome/css/font-awesome.min.css',
 				'bower_components/mdi/css/materialdesignicons.min.css',
 				'bower_components/animate.css/animate.min.css',
+				'bower_components/lightgallery/dist/css/lightgallery.min.css',
+				'bower_components/lightgallery/dist/css/lg-transitions.min.css',
 				// OWL
 				'bower_components/owl.carousel/dist/assets/owl.carousel.min.css',
 				'bower_components/owl.carousel/dist/assets/owl.theme.default.min.css',
